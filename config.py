a7bcab7cf5f1e4c397e34134a200f181e9c2c5bd
Todd Birchard <toddbirchard@gmail.com>
2020-02-16 17:32:18 -0500

    transform most recent images

--- config.py
+++ config.py
@@ -1,9 +1,15 @@
 """Google Cloud Storage Configuration."""
 from os import environ
+import datetime
 
 
 # Google Cloud Storage
 bucket_URL = environ.get('GCP_BUCKET_URL')
 bucket_name = environ.get('GCP_BUCKET_NAME')
 bucket_folder = environ.get('GCP_BUCKET_FOLDER_NAME')
-bucket_image_folder_prefixes = [2019, 2018]
+
+# Current month/year
+dt = datetime.datetime.today()
+year = dt.year
+month = dt.strftime('%m')
+bucket_image_folder_prefixes = [f'{year}/{month}']
